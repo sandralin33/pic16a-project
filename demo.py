fd774b681eaf56c4e427d67f2912cf03b6fcd562
Sandra Lin <114253510+sandralin33@users.noreply.github.com>
2022-11-30 10:49:24 -0800

    create format

--- demo.py
+++ demo.py
@@ -1,1 +1,57 @@
 # create demo file
+
+# explain this
+class File(self):
+    '''
+    none
+    '''
+    
+    # explain this
+    def __init__(self):
+        '''
+        Args:
+            none
+        Returns:
+            None
+        '''
+        # check if CSV file
+        if ##
+            raise TypeError # if not, raise TypeError
+    
+    
+    # explain this
+    def clean_df(self):
+        '''
+        Args:
+            none
+        Returns:
+            None
+        '''
+        pass
+    
+    
+    
+    # explain this
+    def LogReg(self):
+        '''
+        Args:
+            none
+        Returns:
+            none
+        '''
+        pass
+    
+    
+    
+    # explain this
+    def graph(self):
+        '''
+        Args:
+            none
+        Returns:
+            none
+        '''
+        pass
+      
+      
+# modify your "section"
